4f8c60b3fffadf96a24914b34a98e0c39d8e7ceb
prashob-earlyjobs <prashob@earlyjobs.in>
2025-11-17 15:03:53 +0530

    google tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q3V8KPR351"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Q3V8KPR351');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
